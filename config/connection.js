713e0ad6e338197d55ec61c4e0dc4508777335ca
giannifontanot <giannifontanot@gmail.com>
2021-09-19 21:59:24 -0500

    Code may be used in a future version.

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,8 @@ const path = require('path');
 require('dotenv').config({path: root + path.sep + ".env"});
 
 
-
+// THIS CODE IS NOT USED IN THIS APP VERSION
+// I AM PLANNING TO IMPLEMENT IT IN THE FUTURE
 module.exports = connection = () => {
     const db = mongoose.createConnection(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
         reconnectInterval: 5000,
